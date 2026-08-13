a6f518c1535bf5e8d4751a99d0e28a6c375feaa7
Fulan <Fulan@gmail.com>
2026-08-13 12:46:04 +0700

    fix supabase config

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+import {
+  PUBLIC_SUPABASE_URL,
+  PUBLIC_SUPABASE_PUBLISHABLE_KEY
+} from "$env/static/public";
 
 export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
+  PUBLIC_SUPABASE_URL,
+  PUBLIC_SUPABASE_PUBLISHABLE_KEY
 );
